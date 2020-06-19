0c054ce480ae0c4d4edf301aa11db48c0c2b7a63
Steve Westwood <steve.westwood@hexsaw.org.uk>
2020-06-19 23:04:42 +0100

    Rename property file

--- src/main/java/uk/org/hexsaw/logactaesque/PlayFixtureConfiguration.java
+++ src/main/java/uk/org/hexsaw/logactaesque/PlayFixtureConfiguration.java
@@ -4,13 +4,22 @@ import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
+import org.springframework.cloud.aws.messaging.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class PlayFixtureConfiguration {
 
+    @Bean
+    @Primary
     public AmazonSQSAsync sqsClient() {
         InstanceProfileCredentialsProvider provider
                 = new InstanceProfileCredentialsProvider(true);
@@ -24,8 +33,31 @@ public class PlayFixtureConfiguration {
     }
 
     @Bean
-    public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQSAsync) {
-        return new QueueMessagingTemplate(amazonSQSAsync);
+    @ConfigurationProperties(prefix = "aws.sqs.queue")
+    public SimpleMessageListenerContainer simpleMessageListenerContainer(AmazonSQSAsync amazonSQSAsync) {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setAmazonSqs(amazonSQSAsync);
+        simpleMessageListenerContainer.setMessageHandler(queueMessageHandler());
+        simpleMessageListenerContainer.setMaxNumberOfMessages(10);
+        simpleMessageListenerContainer.setTaskExecutor(threadPoolTaskExecutor());
+        return simpleMessageListenerContainer;
+    }
+    
+    @Bean
+    public QueueMessageHandler queueMessageHandler() {
+        QueueMessageHandlerFactory queueMessageHandlerFactory = new QueueMessageHandlerFactory();
+        queueMessageHandlerFactory.setAmazonSqs(sqsClient());
+        QueueMessageHandler queueMessageHandler = queueMessageHandlerFactory.createQueueMessageHandler();
+        return queueMessageHandler;
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(10);
+        executor.initialize();
+        return executor;
     }
 
 }
